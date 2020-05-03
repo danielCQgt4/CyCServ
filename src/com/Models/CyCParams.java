c73bd5789f7a968e56cced70f41a5c66f3adf25a
Daniel Coto Quiros <danielcotoquiros@hotmail.com>
2020-05-02 23:42:23 -0600

    Commit json content-type error hanldling

--- src/com/Models/CyCParams.java
+++ src/com/Models/CyCParams.java
@@ -5,14 +5,24 @@ import java.util.HashMap;
 public class CyCParams<K, V> extends HashMap<K, V> {
 
     public Object getData(Object k) {
-        return this.get(k);
+        Object o = this.get(k);
+        if (o == null) {
+            return "[no-data]";
+        } else {
+            return this.get(k);
+        }
     }
 
     public CyCParams<Object, Object> getObject(Object k) {
         CyCParams<Object, Object> re = new CyCParams<>();
-        ((HashMap<Object, Object>) this.getData(k)).forEach((key, value) -> {
-            re.put(key, value);
-        });
-        return re;
+        try {
+            ((HashMap<Object, Object>) this.getData(k)).forEach((key, value) -> {
+                re.put(key, value);
+            });
+        } catch (Exception e) {
+            System.out.println(k + "->[no-object]");
+        } finally {
+            return re;
+        }
     }
 }
